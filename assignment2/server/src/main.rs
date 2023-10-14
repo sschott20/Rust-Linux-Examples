16e8e11582564dbbfaa4164da4addea5d27e8d70
Alex <sebalexschott@gmail.com>
2023-10-14 17:41:11 +0000

    sending code to local VM

--- assignment2/server/src/main.rs
+++ assignment2/server/src/main.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind("10.0.2.15:7878").unwrap();
+    let listener = TcpListener::bind("10.0.2.15:69420").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
