e3dd43d1b8936b69d603cce15807b67135ef7e44
Alex <sebalexschott@gmail.com>
2023-10-14 21:57:36 +0000

    sending code to local VM

--- assignment2/client/src/main.rs
+++ assignment2/client/src/main.rs
@@ -1,3 +1,6 @@
+use opencv::core::{flip, Vec3b};
+use opencv::videoio::*;
+use opencv::{highgui::*, prelude::*, videoio};
 use std::io::prelude::*;
 use std::net::TcpStream;
 
@@ -7,9 +10,37 @@ fn main() -> std::io::Result<()> {
     // let mut stream = TcpStream::connect("127.0.0.1:8080")?;
     let mut stream = TcpStream::connect("127.0.0.1:54321")?;
 
-    stream.write(&[1])?;
-    stream.read(&mut [0; 128])?;
-    println!("Client finished");
+    // 0 is the default camera
+    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY).unwrap();
+    videoio::VideoCapture::is_opened(&cam).expect("Open camera [FAILED]");
+    cam.set(CAP_PROP_FPS, 30.0)
+        .expect("Set camera FPS [FAILED]");
+
+    loop {
+        let mut frame = Mat::default();
+        cam.read(&mut frame).expect("VideoCapture: read [FAILED]");
+
+        if frame.size().unwrap.width > 0 {
+            // flip the image horizontally
+            let mut flipped = Mat::default();
+            flip(&frame, &mut flipped, 1).expect("flip [FAILED]");
+            // resize the image as a square, size is
+            let resized_img = resize_with_padding(&flipped, [192, 192]);
+
+            // turn Mat into Vec<u8>
+            let vec_2d: Vec<Vec<Vec3b>> = resized_img.to_vec_2d().unwrap();
+            let vec_1d: Vec<u8> = vec_2d
+                .iter()
+                .flat_map(|v| v.iter().flat_map(|w| w.as_slice()))
+                .cloned()
+                .collect();
+            println!("vec_1d.len(): {}", vec_1d.len());
+        }
+
+
+    }
+
+    println!("Client exit");
 
     Ok(())
 }
