5d9e66ffa04273acc0cf7345bd2ca91579a3c152
Alex <sebalexschott@gmail.com>
2023-10-14 16:54:20 +0000

    client server

---
+++ assignment2/server/target/debug/deps/server-5c8dda6871dbd2f0.d
@@ -0,0 +1,5 @@
+/home/alex/cpsc429_schott/assignment2/server/target/debug/deps/server-5c8dda6871dbd2f0: src/main.rs
+
+/home/alex/cpsc429_schott/assignment2/server/target/debug/deps/server-5c8dda6871dbd2f0.d: src/main.rs
+
+src/main.rs:
---
+++ assignment2/server/target/debug/server.d
@@ -0,0 +1,1 @@
+/home/alex/cpsc429_schott/assignment2/server/target/debug/server: /home/alex/cpsc429_schott/assignment2/server/src/main.rs
