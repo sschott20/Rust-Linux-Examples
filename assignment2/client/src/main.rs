510e5b29fdb387e8f3921ffc7a1b1889c15403b7
Alex <sebalexschott@gmail.com>
2023-10-14 17:42:23 +0000

    sending code to local VM

--- assignment2/client/src/main.rs
+++ assignment2/client/src/main.rs
@@ -2,7 +2,7 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("10.0.2.15:69420")?;
+    let mut stream = TcpStream::connect("10.0.2.15:6969")?;
 
     stream.write(&[1])?;
     stream.read(&mut [0; 128])?;
