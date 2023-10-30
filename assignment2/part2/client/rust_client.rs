795016c82d137687a0fbaedf999c855a57088ddd
Alex <sebalexschott@gmail.com>
2023-10-30 23:30:40 +0000

    update

--- assignment2/part2/client/rust_client.rs
+++ assignment2/part2/client/rust_client.rs
@@ -1,19 +1,38 @@
+// SPDX-License-Identifier: GPL-2.0
+
+//! Rust minimal sample.
+
 use kernel::prelude::*;
 
 module! {
     type: Client,
-    name: "client",
-    author: "Alex Schott",
+    name: "rust_client",
+    author: "Rust for Linux Contributors",
+    description: "Rust minimal sample",
     license: "GPL",
 }
-struct Client;
+
+struct Client {
+    numbers: Vec<i32>,
+}
 
 impl kernel::Module for Client {
     fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
-        // Print a banner to make sure our module is working
-        pr_info!("------------------------\n");
-        pr_info!("starting virtual device!\n");
-        pr_info!("------------------------\n");
-        Ok(Client)
+        pr_info!("Rust client (init)\n");
+        pr_info!("Am I built-in? {}\n", !cfg!(MODULE));
+
+        let mut numbers = Vec::new();
+        numbers.try_push(72)?;
+        numbers.try_push(108)?;
+        numbers.try_push(200)?;
+
+        Ok(Client { numbers })
+    }
+}
+
+impl Drop for Client {
+    fn drop(&mut self) {
+        pr_info!("My numbers are {:?}\n", self.numbers);
+        pr_info!("Rust minimal sample (exit)\n");
     }
 }
