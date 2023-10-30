673e6070733d17866b9e77492e6b0c48e1e89994
Alex <sebalexschott@gmail.com>
2023-10-30 18:09:31 +0000

    update

---
+++ assignment2/part2/server/client/client.rs
@@ -0,0 +1,19 @@
+use kernel::prelude::*;
+
+module! {
+    type: Client,
+    name: b"vdev",
+    license: b"GPL",
+}
+
+struct Client;
+
+impl kernel::Module for Client {
+    fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
+        // Print a banner to make sure our module is working
+        pr_info!("------------------------\n");
+        pr_info!("starting virtual device!\n");
+        pr_info!("------------------------\n");
+        Ok(Client)
+    }
+}
