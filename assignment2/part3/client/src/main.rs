8249435f79210a4ca6e2d97336cdbf068f14a43f
Alex <sebalexschott@gmail.com>
2023-10-31 00:52:29 +0000

    update

--- assignment2/part3/client/src/main.rs
+++ assignment2/part3/client/src/main.rs
@@ -4,6 +4,7 @@ use std::{fs::File, os::unix::prelude::AsRawFd, str};
 
 const VIDIOC_QUERYCAP_MAGIC: u8 = 'V' as u8;
 const VIDIOC_QUERYCAP_TYPE_MODE: u8 = 0;
+// const VIDIO_
 
 #[repr(C)]
 #[derive(Default)]
@@ -14,6 +15,20 @@ pub struct v4l2_capability {
     pub version: u32,
     pub capabilities: u32,
     pub device_caps: u32,
+    pub reserved: [u32; 3],
+}
+
+#[repr(C)]
+#[derive(Default)]
+pub struct v4l2_input {
+    pub index: u32,
+    pub name: [u8; 32],
+    pub type_: u32,
+    pub audioset: u32,
+    pub tuner: u32,
+    pub std: u32,
+    pub status: u32,
+    pub capabilities: u32,
     pub reserved: [u32; 3],
 }
 
@@ -44,12 +59,34 @@ fn main() {
             println!("version: {:?}", info.version);
             println!("capabilities: {:?}", info.capabilities);
             println!("device_caps: {:?}", info.device_caps);
-            
         }
         Err(e) => {
             println!("get info [FAILED]: {:?}", e);
         }
     }
 
-    println!("driver: {:?}", str::from_utf8(&info.driver));
+    let mut info: v4l2_input = Default::default();
+
+    ioctl_read!(
+        videoc_g_input,
+        VIDIOC_QUERYCAP_MAGIC,
+        VIDIOC_QUERYCAP_TYPE_MODE,
+        info
+    );
+    match unsafe { videoc_g_input(media_fd, &mut info as *mut v4l2_input) } {
+        Ok(_) => {
+            println!("get info [OK]");
+            println!("driver: {:?}", str::from_utf8(&info.driver));
+            println!("card: {:?}", str::from_utf8(&info.card));
+            println!("bus_info: {:?}", str::from_utf8(&info.bus_info));
+            println!("version: {:?}", info.version);
+            println!("capabilities: {:?}", info.capabilities);
+            println!("device_caps: {:?}", info.device_caps);
+        }
+        Err(e) => {
+            println!("get info [FAILED]: {:?}", e);
+        }
+    }
+
+    println!("Client exit [OK]");
 }
