9f7aeb85436d5107f256ce14f6e0d96c2b0194f0
Alex <sebalexschott@gmail.com>
2023-11-01 00:16:32 +0000

    update

--- assignment2/part3/client/src/main.rs
+++ assignment2/part3/client/src/main.rs
@@ -95,14 +95,18 @@ fn main() {
     ioctl_readwrite!(vidio_enum_fmt, VIDIOC_MAGIC, 2, v4l2_fmtdesc);
     let mut fmtdesc: v4l2_fmtdesc = unsafe { std::mem::zeroed() };
     fmtdesc.type_ = 1;
-    match unsafe { vidio_enum_fmt(media_fd, &mut fmtdesc) } {
-        Ok(_) => {
-            println!("get vidio_enum_fmt [OK]");
-            println!("description: {:?}", str::from_utf8(&fmtdesc.description));
-            println!("pixelformat: {:?}", fmtdesc.pixelformat);
-        }
-        Err(e) => {
-            println!("get vidio_enum_fmt [FAILED]: {:?}", e);
+    loop {
+        match unsafe { vidio_enum_fmt(media_fd, &mut fmtdesc) } {
+            Ok(_) => {
+                println!("get vidio_enum_fmt [OK]");
+                println!("description: {:?}", str::from_utf8(&fmtdesc.description));
+                println!("pixelformat: {:?}", fmtdesc.pixelformat);
+                fmtdesc.index = fmtdesc.index + 1;
+            }
+            Err(e) => {
+                println!("get vidio_enum_fmt [FAILED]: {:?}", e);
+                break;
+            }
         }
     }
 
