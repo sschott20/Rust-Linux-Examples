fec06256a32325b37fcfa51952ddbdf4403f819d
Alex <sebalexschott@gmail.com>
2023-11-01 20:08:26 +0000

    update

--- assignment2/part3/client/src/main.rs
+++ assignment2/part3/client/src/main.rs
@@ -80,6 +80,7 @@ fn stream_on(media_fd: i32) {
         }
     }
 }
+const SIZE: u64 = 1024 * 1024;
 
 fn main() {
     let mut file = File::options()
@@ -95,7 +96,7 @@ fn main() {
     let mut reqbuff: v4l2_requestbuffers = request_buffer(media_fd);
     let mut buf: v4l2_buffer = query_buffer(media_fd);
     let mut stream_on = stream_on(media_fd);
-    // file.seek(SeekFrom::Start(0)).unwrap();
+    file.seek(SeekFrom::Start(SIZE)).unwrap();
     file.write_all(&[0]).unwrap();
     file.seek(SeekFrom::Start(0)).unwrap();
 
