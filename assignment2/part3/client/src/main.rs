9c8bf4e897a3aca2780ab78387826f3b4f2286fb
Alex <sebalexschott@gmail.com>
2023-11-01 18:54:50 +0000

    update

--- assignment2/part3/client/src/main.rs
+++ assignment2/part3/client/src/main.rs
@@ -98,8 +98,6 @@ fn main() {
     buf.memory = 1;
     buf.index = 0;
 
-    use std::{thread, time};
-
     let ten_millis = time::Duration::from_millis(1000);
     let now = time::Instant::now();
     thread::sleep(ten_millis);
