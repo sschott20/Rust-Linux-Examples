aa25bde4ce7786fbd7234889abc90d67edf07bc5
Alex <sebalexschott@gmail.com>
2023-11-02 22:49:24 +0000

    update

--- assignment2/part3/client/src/main.rs
+++ assignment2/part3/client/src/main.rs
@@ -76,12 +76,13 @@ fn main() {
         println!("size: {:?}", b.len());
         let mut testm = opencv::imgcodecs::imread("download.jpg", 1).unwrap();
 
-        println!("size: {:?}", testm.size().unwrap());
-        // let mut testm: Mat =
-        //     Mat::new_rows_cols_with_default(480, 640, Vec3b::typ(), opencv::core::Scalar::all(0.0))
+        // println!("size: {:?}", testm.size().unwrap());
+        let mut testm: Mat =
+            Mat::new_rows_cols_with_default(480, 640, Vec3b::typ(), opencv::core::Scalar::all(0.0))
         //         .unwrap();
+        println!("testm : {:?}", testm);
         // let mut mat: Mat = opencv::imgcodecs::imdecode(&b, -1).unwrap();
-        opencv::highgui::imshow("test", &mut testm).unwrap();
+        opencv::highgui::imshow("test", &mut testm).expect("imshow [error]");
 
         // let _ = s.send(&client.buffer);
 
