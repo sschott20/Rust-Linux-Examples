9db76772b8c8cda852bb8040a8782dc544b2e62d
Alex <sebalexschott@gmail.com>
2023-11-02 20:44:03 +0000

    update

--- assignment2/part3/client/src/server.rs
+++ assignment2/part3/client/src/server.rs
@@ -1,71 +1,71 @@
-#![allow(unused_imports)]
+// #![allow(unused_imports)]
 
-use opencv::core::{flip, Mat, Vec3b, Vector};
-use opencv::videoio::*;
-use opencv::{highgui::*, imgcodecs, prelude::*, videoio};
+// use opencv::core::{flip, Mat, Vec3b, Vector};
+// use opencv::videoio::*;
+// use opencv::{highgui::*, imgcodecs, prelude::*, videoio};
 
-mod utils;
-use std::{thread, time::Duration};
-use utils::*;
+// mod utils;
+// use std::{thread, time::Duration};
+// use utils::*;
 
-use std::io::prelude::*;
-use std::io::{Read, Write};
-use std::net::TcpStream;
+// use std::io::prelude::*;
+// use std::io::{Read, Write};
+// use std::net::TcpStream;
 
 
 
-struct Server {
-    stream: TcpStream,
-}
+// struct Server {
+//     stream: TcpStream,
+// }
 
-impl Server {
-    fn send(&mut self, frame: Mat) -> Mat {
-        // let mut stream = TcpStream::connect("127.0.0.1:54321").unwrap();
-        let mut buffer: Vector<u8> = Vec::new().into();
+// impl Server {
+//     fn send(&mut self, frame: Mat) -> Mat {
+//         // let mut stream = TcpStream::connect("127.0.0.1:54321").unwrap();
+//         let mut buffer: Vector<u8> = Vec::new().into();
 
-        let _ = opencv::imgcodecs::imencode(".bmp", &frame, &mut buffer, &Vector::new());
+//         let _ = opencv::imgcodecs::imencode(".bmp", &frame, &mut buffer, &Vector::new());
 
-        let buffer: Vec<u8> = buffer.to_vec();
-        // self.stream.rewind().unwrap();
-        self.stream.write_all(&buffer).unwrap();
-        // println!("buffer send size: {}", buffer.len());
+//         let buffer: Vec<u8> = buffer.to_vec();
+//         // self.stream.rewind().unwrap();
+//         self.stream.write_all(&buffer).unwrap();
+//         // println!("buffer send size: {}", buffer.len());
 
-        let mut buffer: Vec<u8> = vec![0; 110646];
-        self.stream.read_exact(&mut buffer).unwrap();
-        // println!("buffer recieve size: {}", buffer.len());
+//         let mut buffer: Vec<u8> = vec![0; 110646];
+//         self.stream.read_exact(&mut buffer).unwrap();
+//         // println!("buffer recieve size: {}", buffer.len());
 
-        let mut flipped = Mat::default();
+//         let mut flipped = Mat::default();
 
-        opencv::imgcodecs::imdecode_to(
-            &opencv::types::VectorOfu8::from_iter(buffer),
-            -1,
-            &mut flipped,
-        )
-        .unwrap();
-        let flipped = resize_with_padding(&flipped, [196 * 2, 196 * 2]);
-        flipped
-    }
-}
-fn main() {
-    let mut server = Server {
-        stream: TcpStream::connect("127.0.0.1:54321").unwrap(),
-    };
+//         opencv::imgcodecs::imdecode_to(
+//             &opencv::types::VectorOfu8::from_iter(buffer),
+//             -1,
+//             &mut flipped,
+//         )
+//         .unwrap();
+//         let flipped = resize_with_padding(&flipped, [196 * 2, 196 * 2]);
+//         flipped
+//     }
+// }
+// fn main() {
+//     let mut server = Server {
+//         stream: TcpStream::connect("127.0.0.1:54321").unwrap(),
+//     };
 
-    let mut app = App {
-        cam: videoio::VideoCapture::new(0, videoio::CAP_ANY).unwrap(),
-    };
+//     let mut app = App {
+//         cam: videoio::VideoCapture::new(0, videoio::CAP_ANY).unwrap(),
+//     };
 
-    app.init();
+//     app.init();
 
-    loop {
-        let mut frame = app.read();
-        let mut flipped = server.send(frame);
+//     loop {
+//         let mut frame = app.read();
+//         let mut flipped = server.send(frame);
 
-        imshow("MoveNet", &flipped).expect("imshow [ERROR]");
+//         imshow("MoveNet", &flipped).expect("imshow [ERROR]");
 
-        let key = wait_key(1).unwrap();
-        if key > 0 && key != 255 {
-            break;
-        }
-    }
-}
+//         let key = wait_key(1).unwrap();
+//         if key > 0 && key != 255 {
+//             break;
+//         }
+//     }
+// }
