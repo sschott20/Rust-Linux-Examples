344a825dff6e02c58bba53f7a949aac3192c86ca
Alex <sebalexschott@gmail.com>
2023-11-02 22:42:34 +0000

    update

--- assignment2/part3/client/src/main.rs
+++ assignment2/part3/client/src/main.rs
@@ -79,7 +79,7 @@ fn main() {
             Mat::new_rows_cols_with_default(480, 640, Vec3b::typ(), opencv::core::Scalar::all(0.0))
                 .unwrap();
         // let mut mat: Mat = opencv::imgcodecs::imdecode(&b, -1).unwrap();
-        opencv::highgui::imshow("test", &mut tesm);
+        opencv::highgui::imshow("test", &mut testm);
         // let _ = s.send(&client.buffer);
 
         client.qbuf();
