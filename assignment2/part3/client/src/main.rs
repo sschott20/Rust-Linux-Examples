8c0a7c4c83e33da83999215c55090ae680a1d1b5
Alex <sebalexschott@gmail.com>
2023-11-02 22:45:13 +0000

    update

--- assignment2/part3/client/src/main.rs
+++ assignment2/part3/client/src/main.rs
@@ -74,10 +74,10 @@ fn main() {
         let mut b: Vec<u8> = vec![0; client.buf.bytesused as usize];
         // b.copy_from_slice(&client.buffer);
         println!("size: {:?}", b.len());
-
-        let mut testm: Mat =
-            Mat::new_rows_cols_with_default(480, 640, Vec3b::typ(), opencv::core::Scalar::all(0.0))
-                .unwrap();
+        let mut testm = opensv::imgcodecs::imread("download.jpg", 1).unwrap();
+        // let mut testm: Mat =
+        //     Mat::new_rows_cols_with_default(480, 640, Vec3b::typ(), opencv::core::Scalar::all(0.0))
+        //         .unwrap();
         // let mut mat: Mat = opencv::imgcodecs::imdecode(&b, -1).unwrap();
         opencv::highgui::imshow("test", &mut testm).unwrap();
         // let _ = s.send(&client.buffer);
