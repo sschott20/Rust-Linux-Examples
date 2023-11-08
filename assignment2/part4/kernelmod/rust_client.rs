41caedf0c4ac1eaaaa99349ff3fb0654c1e89c12
Alex <sebalexschott@gmail.com>
2023-11-08 23:50:37 +0000

    update

--- assignment2/part4/kernelmod/rust_client.rs
+++ assignment2/part4/kernelmod/rust_client.rs
@@ -3,62 +3,45 @@
 //! Rust minimal sample.
 #![allow(missing_docs)]
 #![allow(unused_imports)]
+use kernel::bindings::{filp_open, vfs_ioctl};
+use kernel::file::{File, Operations};
 use kernel::prelude::*;
 use kernel::sync::smutex::Mutex;
+use kernel::{miscdev, Module};
+// use kernel::str::CStr;
 
 module! {
-    type: Mdriverf,
-    name: "mdriverf",
+    type: RustClient,
+    name: "rust_client",
     author: "Alex Schott",
     license: "GPL",
 }
 
-struct Mdriverf;
+struct RustClient {
+    _dev: Pin<Box<miscdev::Registration<RustClient>>>,
+}
 
-static MEM_BUFFER: Mutex<[u8; 512000]> = Mutex::new([0u8; 512000]);
-
-impl kernel::Module for Mdriverf {
+impl kernel::Module for RustClient {
     fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
-        pr_info!("mdriverf (init)\n");
-
-        Ok(Mdriverf {})
+        pr_info!("rust_client init (init)\n");
+        let reg = miscdev::Registration::new_pinned(fmt!("rust_client"), ())?;
+        Ok(RustClient { _dev: reg })
     }
 }
+#[vtable]
+impl Operations for RustClient {
+    fn open(_context: &(), _file: &File) -> Result {
+        pr_info!("RustClient was opened\n");
+        unsafe {
+            let c_str = CStr::from_bytes_with_nul(b"/dev/video2\0").unwrap();
+            let _file = filp_open(c_str.as_ptr() as *const i8, 0, 0);
+        }
 
-pub fn read(outbuf: &mut [u8], offset: usize) -> usize {
-    // pr_info!("read\n");
-
-    let mut i = 0;
-    let mem_guard = MEM_BUFFER.lock();
-
-    while i < outbuf.len() {
-        // if offset + i >= 512000 {
-        //     return Err(EINVAL);
-        // }
-        outbuf[i] = mem_guard[offset + i];
-        i += 1;
+        Ok(())
     }
-    return i;
 }
-
-pub fn write(inbuf: &[u8], offset: usize) -> usize {
-    // pr_info!("write\n");
-    let mut i = 0;
-    let mut mem_guard = MEM_BUFFER.lock();
-
-    while i < inbuf.len() {
-        // if offset + i >= 512000 {
-        //     return Err(EINVAL);
-        // }
-        mem_guard[offset + i] = inbuf[i];
-        i += 1;
-    }
-
-    return i;
-}
-
-impl Drop for Mdriverf {
+impl Drop for RustClient {
     fn drop(&mut self) {
-        pr_info!("Rust mdriver (exit)\n");
+        pr_info!("RustClient (exit)\n");
     }
 }
