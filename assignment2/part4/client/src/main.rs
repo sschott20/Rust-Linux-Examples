83781ae4d71b1e14bd135d89259fab3904674b36
Alex <sebalexschott@gmail.com>
2023-11-13 21:55:42 +0000

    update

--- assignment2/part4/client/src/main.rs
+++ assignment2/part4/client/src/main.rs
@@ -132,7 +132,7 @@ fn main() -> io::Result<()> {
     // let mut buffer: Vec<u8> = Vec::with_capacity(110646);
     // let mut tmp_buf: [u8; 110646] = [0; 110646];
     client.read();
-    let len = f.read_exact(&mut tmp_buf);
+    let len = f.read_exact(&mut buffer);
     // buffer.extend_from_slice(&tmp_buf);
     // let len = f.read_exact(&mut buffer);
     // print first 10 bytes from buffer:
