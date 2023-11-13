abeb42999854fbb6d519512eec67a83175d80c9c
Alex <sebalexschott@gmail.com>
2023-11-13 16:27:44 +0000

    update

--- assignment2/part4/client/src/main.rs
+++ assignment2/part4/client/src/main.rs
@@ -129,7 +129,7 @@ fn main() -> io::Result<()> {
     // loop {
     let mut buffer = [0; 110646];
 
-    // f.read(&mut tmp)?;
+    f.read(&mut tmp)?;
     // let mut buffer: Vec<u8> = vec![0; 110646];
 
     // f.read(&mut buffer)?;
