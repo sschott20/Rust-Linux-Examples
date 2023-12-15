5720be3eb92ee26510e47d930a89f2f4bea23cf2
Alex <sebalexschott@gmail.com>
2023-12-15 21:05:17 +0000

    update

--- assignment3/client/src/main.rs
+++ assignment3/client/src/main.rs
@@ -1,88 +1,92 @@
-#![allow(unused_mut)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use std::{thread, time};
-mod utils;
-
-use nix;
-use nix::ioctl_write_ptr;
-mod bindings;
-use bindings::*;
-use opencv::core::{flip, Mat, Vec3b};
+use opencv::core::{flip, Mat, Vec3b, Vector};
 use opencv::videoio::*;
-use opencv::{highgui::*, prelude::*, videoio};
-use std::net::TcpStream;
+use opencv::{highgui::*, imgcodecs, prelude::*, videoio};
 
-use memmap::Mmap;
-use memmap::MmapOptions;
-mod server;
-mod setup;
-use nix::sys::ioctl;
-use nix::sys::select;
-use nix::sys::select::FdSet;
-use server::*;
-use setup::*;
-mod app;
-use app::*;
-use std::{fs::File, os::unix::prelude::AsRawFd, str};
-use std::{
-    fs::OpenOptions,
-    io::{Seek, SeekFrom, Write},
-};
+mod utils;
+use std::{thread, time::Duration};
 use utils::*;
 
+use std::io::prelude::*;
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+struct App {
+    cam: videoio::VideoCapture,
+}
+
+impl App {
+    fn init(&mut self) {
+        self.cam
+            .set(CAP_PROP_FPS, 30.0)
+            .expect("Set camera FPS [FAILED]");
+        videoio::VideoCapture::is_opened(&self.cam).expect("Open camera [FAILED]");
+    }
+
+    fn read(&mut self) -> Mat {
+        let mut frame = Mat::default();
+        self.cam
+            .read(&mut frame)
+            .expect("VideoCapture: read [FAILED]");
+        // resize the image as a square, size is
+        let mut flipped = Mat::default();
+        flip(&frame, &mut flipped, 1).expect("flip [FAILED]");
+        let resized_img = resize_with_padding(&flipped, [192, 192]);
+        resized_img
+    }
+}
+
+struct Server {
+    stream: TcpStream,
+}
+
+impl Server {
+    fn send(&mut self, frame: Mat) -> Mat {
+        // let mut stream = TcpStream::connect("127.0.0.1:54321").unwrap();
+        let mut buffer: Vector<u8> = Vec::new().into();
+
+        let _ = opencv::imgcodecs::imencode(".bmp", &frame, &mut buffer, &Vector::new());
+
+        let buffer: Vec<u8> = buffer.to_vec();
+        // self.stream.rewind().unwrap();
+        self.stream.write_all(&buffer).unwrap();
+        // println!("buffer send size: {}", buffer.len());
+
+        let mut buffer: Vec<u8> = vec![0; 110646];
+        self.stream.read_exact(&mut buffer).unwrap();
+        // println!("buffer recieve size: {}", buffer.len());
+
+        let mut flipped = Mat::default();
+
+        opencv::imgcodecs::imdecode_to(
+            &opencv::types::VectorOfu8::from_iter(buffer),
+            -1,
+            &mut flipped,
+        )
+        .unwrap();
+        let flipped = resize_with_padding(&flipped, [196 * 2, 196 * 2]);
+        flipped
+    }
+}
 fn main() {
-    let mut f = File::options()
-        .write(true)
-        .read(true)
-        .open("/dev/video2")
-        .unwrap();
-
-    let mut fd = f.as_raw_fd();
-
-    // im sorry for this one and I hate borrow rules so much
-    let mut ftmp = File::options()
-        .write(true)
-        .read(true)
-        .create(true)
-        .open("tmp")
-        .unwrap();
-
-    let mut client: App = App {
-        buffer: unsafe { memmap::MmapOptions::new().len(1).map_mut(&ftmp).unwrap() },
-        file: f,
-        buf: unsafe { std::mem::zeroed() },
-        media_fd: fd,
-    };
-    client.start_device(3);
-
-    client.buffer = unsafe {
-        memmap::MmapOptions::new()
-            .len(client.buf.length as usize)
-            .map_mut(&client.file)
-            .unwrap()
+    let mut server = Server {
+        stream: TcpStream::connect("127.0.0.1:54321").unwrap(),
     };
 
-    let mut s = Server {
-        stream: TcpStream::connect("127.0.0.1:54321").expect("failed to connect"),
+    let mut app = App {
+        cam: videoio::VideoCapture::new(0, videoio::CAP_ANY).unwrap(),
     };
-    let mut i = 0;
+
+    app.init();
+
     loop {
-        if i > 10000 {
-            break;
-        }
-        i = i + 1;
+        let mut frame = app.read();
+        let mut flipped = server.send(frame);
 
-        client.read();
-        let frame: Mat = s.send(&client.buffer);
-        imshow("MoveNet", &frame).expect("imshow [ERROR]");
+        imshow("MoveNet", &flipped).expect("imshow [ERROR]");
+
         let key = wait_key(1).unwrap();
         if key > 0 && key != 255 {
             break;
         }
-
-        client.qbuf();
     }
-
-    std::fs::remove_file("tmp");
 }
