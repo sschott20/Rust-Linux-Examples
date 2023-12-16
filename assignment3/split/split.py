b229a6892c2c22dec1d11cbac31a7003dd33a8c7
Alex <sebalexschott@gmail.com>
2023-12-16 18:26:34 +0000

    update

--- assignment3/split/split.py
+++ assignment3/split/split.py
@@ -15,43 +15,35 @@ if __name__ == '__main__':
         original_model = md.ModelT.InitFromObj(original_model)
         lower_model = md.ModelT.InitFromObj(lower_model)
         upper_model = md.ModelT.InitFromObj(upper_model)
-        
-        original_subgraphs = original_model.subgraphs
-        upper_subgraphs = upper_model.subgraphs
-        lower_subgraphs = lower_model.subgraphs
+    
 
-        original_subgraph = original_subgraphs[0]
-        lower_subgraph = lower_subgraphs[0]
-        upper_subgraph = upper_subgraphs[0]
-
-        upper_operators = upper_subgraph.operators[0:6]
-        lower_operators = lower_subgraph.operators[6:]
+        lower_model.subgraphs[0].operators = original_model.subgraphs[0].operators[:6]
+        upper_model.subgraphs[0].operators = original_model.subgraphs[0].operators[6:]
 
         buf = tflite.Buffer.BufferT()
-        upper_buffers = upper_model.buffers
-        upper_buffers.append(buf)
-
-        lower_buffers = lower_model.buffers
-        lower_buffers.append(buf)
+        upper_model.buffers.append(buf)
+        lower_model.buffers.append(buf)
 
         deq_tensor = ten.TensorT()
-        deq_tensor.shape = [1,96,96,3]
+        deq_tensor.shape = [1,96,96,32]
         deq_tensor.type = 0
         deq_tensor.buffer = 335
         deq_tensor.name = b'StatefulPartitionedCall:0'
-        upper_tensors = upper_subgraph.tensors
-        upper_tensors.append(deq_tensor) 
 
         quant_tensor = ten.TensorT()
-        quant_tensor.shape = [1,96,96,3]
+        quant_tensor.shape = [1,96,96,32]
         quant_tensor.type = 0
         quant_tensor.buffer = 335
         quant_tensor.name = b'StatefulPartitionedCall:0'
-        lower_tensors = lower_subgraph.tensors
-        lower_tensors.append(quant_tensor)
 
-        upper_subgraph.outputs = [333]
-        lower_subgraph.outputs = [333]
+        lower_model.subgraphs[0].tensors.append(deq_tensor)
+        # upper_model.subgraphs[0].tensors = [quant_tensor] + upper_model.subgraphs[0].tensors
+        upper_model.subgraphs[0].tensors.append(quant_tensor)
+
+        # print(upper_model.subgraphs[0].outputs)
+        # print(lower_model.subgraphs[0].outputs)
+        upper_model.subgraphs[0].inputs = [333]
+        lower_model.subgraphs[0].outputs = [333]
 
         deq_op = tflite.Operator.OperatorT()
         deq_op.opcode_index = 13
@@ -63,16 +55,18 @@ if __name__ == '__main__':
         quant_op.inputs = [333]
         quant_op.outputs = [179]
 
-        upper_subgraph.operators.append(deq_op)
-        lower_subgraph.operators.append(quant_op)
+        # upper_model.subgraphs[0].operators = [quant_op] + upper_model.subgraphs[0].operators
+        upper_model.subgraphs[0].operators.append(quant_op)
+        lower_model.subgraphs[0].operators.append(deq_op)
         
         builder = flatbuffers.Builder(1024)
+
         packed = lower_model.Pack(builder)
-        builder.Finish(packed)
+        builder.Finish(packed, b"TFL3")
         with open('lower_model.tflite', 'wb') as f:
             f.write(builder.Output())
-            
+
         packed = upper_model.Pack(builder)
-        builder.Finish(packed)
+        builder.Finish(packed, b"TFL3")
         with open('upper_model.tflite', 'wb') as f:
             f.write(builder.Output())
