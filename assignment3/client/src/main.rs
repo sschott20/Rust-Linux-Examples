4735199d6b2133cdca08242c5a4d246be74e15a9
Alex <sebalexschott@gmail.com>
2023-12-16 19:44:58 +0000

    update

--- assignment3/client/src/main.rs
+++ assignment3/client/src/main.rs
@@ -1,3 +1,120 @@
+#![allow(unused_imports)]
+
+use opencv::core::{flip, Mat, Vec3b, Vector};
+use opencv::videoio::*;
+use opencv::{highgui::*, imgcodecs, prelude::*, videoio};
+
+mod utils;
+use std::vec;
+use std::{thread, time::Duration};
+use utils::*;
+
+use std::io::prelude::*;
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+struct App {
+    cam: videoio::VideoCapture,
+}
+
+impl App {
+    fn init(&mut self) {
+        self.cam
+            .set(CAP_PROP_FPS, 30.0)
+            .expect("Set camera FPS [FAILED]");
+        videoio::VideoCapture::is_opened(&self.cam).expect("Open camera [FAILED]");
+    }
+
+    fn read(&mut self) -> Mat {
+        let mut frame = Mat::default();
+        self.cam
+            .read(&mut frame)
+            .expect("VideoCapture: read [FAILED]");
+        // resize the image as a square, size is
+        let mut flipped = Mat::default();
+        flip(&frame, &mut flipped, 1).expect("flip [FAILED]");
+        let resized_img = resize_with_padding(&flipped, [192, 192]);
+        resized_img
+    }
+}
+
+struct Server {
+    stream: TcpStream,
+}
+
+impl Server {
+    fn send(&mut self, vec_1d: Vec<u8>) -> Mat {
+        // let mut stream = TcpStream::connect("127.0.0.1:54321").unwrap();
+        let mut buffer: Vector<u8> = Vec::new().into();
+
+        // let _ = opencv::imgcodecs::imencode(".bmp", &frame, &mut buffer, &Vector::new());
+
+        // let buffer: Vec<u8> = buffer.to_vec();
+        // self.stream.rewind().unwrap();
+        self.stream.write_all(&vec_1d).unwrap();
+        // println!("buffer send size: {}", buffer.len());
+
+        let mut buffer: Vec<u8> = vec![0; 204];
+        self.stream.read_exact(&mut buffer).unwrap();
+        // println!("buffer recieve size: {}", buffer.len());
+
+        let mut flipped = Mat::default();
+
+        opencv::imgcodecs::imdecode_to(
+            &opencv::types::VectorOfu8::from_iter(buffer),
+            -1,
+            &mut flipped,
+        )
+        .unwrap();
+        let flipped = resize_with_padding(&flipped, [196 * 2, 196 * 2]);
+        flipped
+    }
+}
 fn main() {
-    println!("Hello,  world!");
+    let mut server = Server {
+        stream: TcpStream::connect("127.0.0.1:54321").unwrap(),
+    };
+
+    let mut app = App {
+        cam: videoio::VideoCapture::new(0, videoio::CAP_ANY).unwrap(),
+    };
+
+    let path: String = format!("resource/lower_model.tflite");
+    let lower_model: Model<'_> = Model::new(&path).expect("Load model [FAILED]");
+    let lower_interpreter: Interpreter<'_> =
+        Interpreter::new(&lower_model, Some(Options::default())).unwrap();
+    lower_interpreter.allocate_tensors().unwrap();
+
+    app.init();
+
+    loop {
+        let mut frame = app.read();
+
+        let mut flipped = Mat::default();
+        flip(&frame, &mut flipped, 1).expect("error");
+        let resized_img = resize_with_padding(&frame, [192, 192]);
+
+        let vec_2d: Vec<Vec<Vec3b>> = resized_img.to_vec_2d().unwrap();
+        let vec_1d: Vec<u8> = vec_2d
+            .iter()
+            .flat_map(|v| v.iter().flat_map(|w| w.as_slice()))
+            .cloned()
+            .collect();
+        // set input (tensor0)
+        lower_interpreter.copy(&vec_1d[..], 0).unwrap();
+        lower_interpreter.invoke().expect("Invoke [FAILED]");
+
+        let output_tensor = lower_interpreter.output(0).unwrap();
+
+        let vec_1d: Vec<u8> = output_tensor.data::<u8>().to_vec();
+        let mut vec_1d: Vec<u8> = server.send(vec_1d);
+        let f32_vec: Vec<f32> = vec_1d.iter().map(|&x| x as f32).collect();
+        draw_keypoints(&mut flipped, vec_1d, 0.25);
+        imshow("MoveNet", &flipped).expect("imshow [ERROR]");
+
+        let key = wait_key(1).unwrap();
+        if key > 0 && key != 255 {
+            break;
+        }
+    }
 }
--- assignment3/server/src/main.rs
+++ assignment3/server/src/main.rs
@@ -24,60 +24,99 @@ use std::{
 use tflitec::interpreter::{Interpreter, Options};
 use tflitec::model::Model;
 use utils::*;
+struct Server {
+    stream: TcpStream,
+}
+impl Server {
+    fn recieve(&mut self) -> Vec<u8> {
+        let mut buffer: Vec<u8> = vec![0; 1179648];
+        self.stream.read_exact(&mut buffer).unwrap();
+        // println!("buffer recieve size: {:?}", buffer.len());
+        buffer
+    }
+    fn send(&mut self, vec: Vec<u8>) {
+        self.stream.write_all(&vec).unwrap();
+    }
+}
+struct App<'a> {
+    interpreter: Interpreter<'a>,
+}
+impl App<'_> {
+    fn dnn(&mut self, mut vec_1d: Vec<u8>) -> Vec<u8> {
+        self.interpreter.copy(&vec_1d[..], 0).unwrap();
+        self.interpreter.invoke().expect("Invoke [FAILED]");
 
+        let output_tensor = self.interpreter.output(0).unwrap();
+
+        let vec_1d: Vec<u8> = output_tensor.data::<u8>().to_vec();
+
+        self.interpreter.copy(&vec_1d, 0).unwrap();
+        self.interpreter.invoke().expect("Invoke [FAILED]");
+
+        let final_output = self.interpreter.output(0).unwrap();
+        let vec_1d_out: Vec<u8> = final_output.data::<u8>().to_vec();
+        vec_1d_out
+    }
+}
 fn main() {
     println!("Server started");
 
-    let path: String = format!("resource/upper_model.tflite");
-    let upper_model: Model<'_> = Model::new(&path).expect("Load model [FAILED]");
+    // let path: String = format!("resource/upper_model.tflite");
+    // let upper_model: Model<'_> = Model::new(&path).expect("Load model [FAILED]");
 
-    let path: String = format!("resource/lower_model.tflite");
-    let lower_model: Model<'_> = Model::new(&path).expect("Load model [FAILED]");
+    // let path: String = format!("resource/lower_model.tflite");
+    // let lower_model: Model<'_> = Model::new(&path).expect("Load model [FAILED]");
 
-    let upper_interpreter: Interpreter<'_> =
-        Interpreter::new(&upper_model, Some(Options::default())).unwrap();
-    let lower_interpreter: Interpreter<'_> =
-        Interpreter::new(&lower_model, Some(Options::default())).unwrap();
+    // let upper_interpreter: Interpreter<'_> =
+    // Interpreter::new(&upper_model, Some(Options::default())).unwrap();
+    // let lower_interpreter: Interpreter<'_> =
+    //     Interpreter::new(&lower_model, Some(Options::default())).unwrap();
 
-    upper_interpreter.allocate_tensors().unwrap();
-    lower_interpreter.allocate_tensors().unwrap();
+    // upper_interpreter.allocate_tensors().unwrap();
+    // lower_interpreter.allocate_tensors().unwrap();
 
-    let mut frame = opencv::imgcodecs::imread("resource/pose.jpg", opencv::imgcodecs::IMREAD_COLOR)
-        .expect("Can't load image");
+    // let mut frame = opencv::imgcodecs::imread("resource/pose.jpg", opencv::imgcodecs::IMREAD_COLOR)
+    //     .expect("Can't load image");
 
-    let mut flipped = Mat::default();
-    flip(&frame, &mut flipped, 1).expect("error");
-    let resized_img = resize_with_padding(&frame, [192, 192]);
+    // let mut flipped = Mat::default();
+    // flip(&frame, &mut flipped, 1).expect("error");
+    // let resized_img = resize_with_padding(&frame, [192, 192]);
 
-    let vec_2d: Vec<Vec<Vec3b>> = resized_img.to_vec_2d().unwrap();
-    let vec_1d: Vec<u8> = vec_2d
-        .iter()
-        .flat_map(|v| v.iter().flat_map(|w| w.as_slice()))
-        .cloned()
-        .collect();
+    // let vec_2d: Vec<Vec<Vec3b>> = resized_img.to_vec_2d().unwrap();
+    // let vec_1d: Vec<u8> = vec_2d
+    //     .iter()
+    //     .flat_map(|v| v.iter().flat_map(|w| w.as_slice()))
+    //     .cloned()
+    //     .collect();
     // set input (tensor0)
-    lower_interpreter.copy(&vec_1d[..], 0).unwrap();
-    lower_interpreter.invoke().expect("Invoke [FAILED]");
 
-    let output_tensor = lower_interpreter.output(0).unwrap();
+    let path = format!("resource/upper_model.tflite");
+    let model = Model::new(&path).expect("Load model [FAILED]");
 
-    let vec_1d: Vec<u8> = output_tensor.data::<u8>().to_vec();
+    let mut app = App {
+        interpreter: Interpreter::new(&model, Some(Options::default()))
+            .expect("Create interpreter [FAILED]"),
+    };
 
-    upper_interpreter.copy(&vec_1d, 0).unwrap();
-    upper_interpreter.invoke().expect("Invoke [FAILED]");
-
-    let final_output = upper_interpreter.output(0).unwrap();
-    draw_keypoints(&mut flipped, final_output.data::<f32>(), 0.25);
-    opencv::imgcodecs::imwrite("resource/poseupdate.jpg", &flipped, &Vector::new()).unwrap();
-    // println!("final_output: {:?}", final_output.shape());
-
+    app.interpreter
+        .allocate_tensors()
+        .expect("Allocate tensors [FAILED]");
     let listener = TcpListener::bind("10.0.2.15:23451").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {}
+            Ok(mut stream) => {
+                let mut server = Server { stream: stream };
+
+                loop {
+                    let mut vec_1d: Vec<u8> = server.recieve();
+                    let mut frame: Vec<u8> = app.dnn(vec_1d);
+                    server.send(frame);
+                }
+            }
             Err(e) => {
                 println!("Error accepting connection: {}", e);
+                // eprintln!("Error accepting connection: {}", e);
             }
         }
     }
